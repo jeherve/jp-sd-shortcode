d2fc097a68af1ceff60abe9a60c4a0b4f522fc54
Jeremy Herve <jeremy@tagada.hu>
2012-12-05 13:20:29 +0100

    Change plugin logic, props westi

--- jp-sd-shortcode.php
+++ jp-sd-shortcode.php
@@ -4,28 +4,29 @@
  * Plugin URI: http://wordpress.org/extend/plugins/jetpack/
  * Description: Extends the Jetpack plugin and allows you to add sharing buttons anywhere inside your posts thanks to the [jpshare] shortcode
  * Author: Jeremy Herve
- * Version: 1.1
+ * Version: 1.2
  * Author URI: http://jeremyherve.com
  * License: GPL2+
  * Text Domain: jetpack
  */
- 
-if (
-	class_exists( 'Jetpack' ) && method_exists( 'Jetpack', 'get_active_modules' ) && in_array( 'sharedaddy', Jetpack::get_active_modules() )
-) :
 
 function tweakjp_sd_shortcode() {
    return sharing_display();
 }
-add_shortcode( 'jpshare', 'tweakjp_sd_shortcode' );
-
-else :
 
 function tweakjp_sd_rm_shortcode( $content ) {
 	return str_replace( '[jpshare]', '', $content );
 }
-add_filter( 'the_content', 'tweakjp_sd_rm_shortcode' );
 
-endif;
+function tweakjp_sd_enable() {
+	if (
+		class_exists( 'Jetpack' ) && method_exists( 'Jetpack', 'get_active_modules' ) && in_array( 'sharedaddy', Jetpack::get_active_modules() )
+		) {
+		add_shortcode( 'jpshare', 'tweakjp_sd_shortcode' );
+	} else {
+		add_filter( 'the_content', 'tweakjp_sd_rm_shortcode' );
+	}
+}
+add_action( 'plugins_loaded', 'tweakjp_sd_enable' );
 
 ?>
